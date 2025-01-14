44ccc74eb0f41924fd656a71ee08354ce2f58751
Milinda John <63205790+milindajay@users.noreply.github.com>
2025-01-14 13:31:18 +0530

    Update header.php
    1. Updated to allow Critical CSS to be inlined if needed(using the `$criticalCSS` variable)
    
    2. Default Page title set to IMBS

--- components/header.php
+++ components/header.php
@@ -4,12 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IMBS LMS - <?php echo $pageTitle ?? ''; ?></title>
+    <title>IMBS LMS - <?php echo $pageTitle ?? 'IMBS LMS'; ?></title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <?php if(isset($useJQuery)): ?>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <?php endif; ?>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght=600&family=Poppins:wght=300&display=swap"
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Poppins:wght@300&display=swap"
         rel="stylesheet">
     <style>
     .heading {
@@ -20,6 +17,12 @@
         font-family: 'Poppins', sans-serif;
     }
     </style>
+    <?php if(isset($criticalCSS)): ?>
+    <style>
+    <?php echo $criticalCSS;
+    ?>
+    </style>
+    <?php endif; ?>
 </head>
 
 <body class="bg-gray-100 flex flex-col min-h-screen">
