5bf76cd9033418fd19280f6243757df529e38efc
Milinda John <63205790+milindajay@users.noreply.github.com>
2025-01-14 13:17:43 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -9,6 +9,5 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    die();
+    die("Connection failed: " . $e->getMessage());
 }
